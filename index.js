6a74c23ed1dd8000b3c276610cbbadeabd8f30cc
SamhainG <gavrilyk.oleg@gmail.com>
2017-06-14 15:50:17 +0300

    Залил исходные файлы

---
+++ index.js
@@ -0,0 +1,11 @@
+"use strict";
+
+/**
+ * Функция для конвертации строк из GET запросов в запрос к ElasticSearch
+ * @param {String} queryString
+ * @returns {{}}
+ */
+module.exports = function QSToES (queryString) {
+  /* @todo реализовать логику */
+  return {};
+};
